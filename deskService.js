f6e03c038a68c68e187d409f9a55508e0ce3ce0e
dewey <d.thorne@gmail.com>
2015-07-10 22:55:29 +0000

    Added servo python files

--- deskService.js
+++ deskService.js
@@ -1,14 +1,24 @@
 // var io = require('pi-gpio');
 var logger = require('simple-node-logger').createSimpleLogger('desk.io.log');
+var exec = require('child_process').exec;
 
 exports.stopDesk = function() {
 	logger.info('deskService stop');
+	exec('python ./servostop.py', function(error, stdout, stderr){
+
+	});
 };
 
 exports.raiseDesk = function() {
 	logger.info('deskService raise');
+	exec('python ./servoup.py', function(error, stdout, stderr){
+
+	});
 };
 
 exports.lowerDesk = function() {
 	logger.info('deskService lower');
+	exec('python ./servodown.py', function(error, stdout, stderr){
+
+	});
 };
